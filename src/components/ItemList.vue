0f2eeaccc03289bfc5b9b8ca2d6456b97d569f36
Oxojo <oxojo9x@gmail.com>
2024-12-04 02:25:08 +0900

    表示を追加

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -14,6 +14,12 @@ const items = ref<Item[]>([
 
 <template>
   <div>ItemList</div>
+  <ul>
+    <li v-for="item in items" :key="item.name">
+      <div>名前: {{ item.name }}</div>
+      <div>{{ item.price }} 円</div>
+    </li>
+  </ul>
 </template>
 
 <style></style>
